aa866be40a121c31e4221125bd19962cc215dd90
James Kuang <70265978+jamersonk@users.noreply.github.com>
2022-06-25 10:32:38 +0800

    add: basic list of tasks.

--- Todo/ContentView.swift
+++ Todo/ContentView.swift
@@ -4,13 +4,20 @@
 //
 //  Created by James Kuang on 25/6/22.
 //
-
 import SwiftUI
 
 struct ContentView: View {
+    
+    var todos = [
+        Todo(title: "Watch some Paw Patrol"),
+        Todo(title: "Conduct a giveaway"),
+        Todo(title: "Randomly deduct some points")
+    ]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        List(todos) { todo in
+            Text(todo.title)
+        }
     }
 }
 
---
+++ Todo/Todo.swift
@@ -0,0 +1,16 @@
+//
+//  Todo.swift
+//  Todo
+//
+//  Created by James Kuang on 25/6/22.
+//
+
+import Foundation
+
+struct Todo: Identifiable {
+    
+    let id = UUID()
+    
+    var title: String
+    var isCompleted = false
+}
