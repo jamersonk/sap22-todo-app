131576f46a674f7246c257308318d8932023eed6
James Kuang <70265978+jamersonk@users.noreply.github.com>
2022-06-25 10:15:42 +0800

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by James Kuang on 25/6/22.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
